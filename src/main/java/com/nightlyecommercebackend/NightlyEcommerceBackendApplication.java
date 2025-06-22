8ae02e61f5a11345046f750e78bc2740effd40be
MounishMadduri <mounish5302@gmail.com>
2025-06-22 16:02:37 +0530

    Initial commit: Spring Boot Setup

---
+++ src/main/java/com/nightlyecommercebackend/NightlyEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nightlyecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightlyEcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NightlyEcommerceBackendApplication.class, args);
+    }
+
+}
